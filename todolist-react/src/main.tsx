96e92595609a629e1058e7f727bae47b7d2113c2
chawanSatita <86059680+chawanSatita@users.noreply.github.com>
2024-06-02 17:43:06 +0700

    edit file

--- todolist-react/src/main.tsx
+++ todolist-react/src/main.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+
 import TodoList from './components/TodoList'
-import TodolistOld from './components/TodolistOld.tsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
